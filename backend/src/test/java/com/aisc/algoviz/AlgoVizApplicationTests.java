9d170768b01ef09a44c60bf604b61325e04993af
hienpham0344 <24520477@gm.uit.edu.vn>
2026-08-21 21:33:01 +0700

    create folder structure for project

---
+++ backend/src/test/java/com/aisc/algoviz/AlgoVizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisc.algoviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoVizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
